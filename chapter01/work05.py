30f0daa4325ff2302ef9405e21b416c887cf611c
sato-no1s <161098649+sato-no1s@users.noreply.github.com>
2026-01-31 11:44:54 +0000

    変更内容を保存

--- chapter01/work05.py
+++ chapter01/work05.py
@@ -1,3 +1,9 @@
 # 複数の例外を受け取る
-
-
+try :
+    print(a)
+except KeyError as e:
+    print("存在しないキーを指定しました:" + str(e))
+except NameError as e:
+    print("変数が存在しません:" + str(e))
+else:
+    print("正しい処理です")
